5811c68527857511177e7a1666523228bd2dbb84
Luis Antonio <14luisdias@gmail.com>
2024-01-28 22:52:42 -0500

    aulas 121-123

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -6,8 +6,11 @@ use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato(){
+    public function contato(Request $request){
         //print_r($_POST);
+        echo'<pre>';
+        print_r($request->all());
+        echo'</pre>';
         $titulo = 'Super Gestão - Contato';
         return view('sites.contato',compact('titulo'));
            
